b97fe717c902287df47714456ad2dc0583373111
Carlos Coto <cmcoto@gmail.com>
2015-06-12 12:01:01 -0600

    Add Carnet and Proyecto to User Account

--- app/user_templates/controllers/accounts_controller.rb
+++ app/user_templates/controllers/accounts_controller.rb
@@ -1,5 +1,16 @@
 module UserTemplates
   class AccountsController < Volt::ModelController
+before_action :require_login
 
+    def index
+      self.model = Volt.current_user.then(&:buffer)
+    end
+
+    def save
+      model.save! do
+        flash._notices << 'Account settings saved'
+        redirect_to '/'
+      end
+    end
   end
 end
